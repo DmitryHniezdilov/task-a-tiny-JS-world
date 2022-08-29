2de9784198ab0a613d7d10d3baf93671f73f869c
DmitryHniezdilov <hniezdilovd@gmail.com>
2022-08-29 17:13:59 +0300

    feat: added inhabitants

--- index.js
+++ index.js
@@ -1,15 +1,67 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
+   Code repository: https://github.com/DmitryHniezdilov/task-a-tiny-JS-world
    Web app: _put project's github pages URL here_
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
 
+const dog = {
+  species: "dog",
+  name: "Togo",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "woof-woof!",
+};
+
+const cat = {
+  species: "cat",
+  name: "Murka",
+  gender: "female",
+  legs: 4,
+  hands: 0,
+  saying: "meow-meow!",
+};
+
+const bear = {
+  species: "character",
+  name: "Winnie",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "Think, think, think!",
+};
+
+const men = {
+  species: "human",
+  name: "Artur",
+  gender: "male",
+  legs: 4,
+  hands: 2,
+  saying: "Do not borrow tomorrow's troubles today.",
+};
+
+const women = {
+  species: "human",
+  name: "Marilyn",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: "A career is born in public – talent in privacy.",
+};
+
+const listOfInhabitants = [dog, cat, bear, men, women];
 
 // ======== OUTPUT ========
+
+listOfInhabitants.forEach(
+  ({ species, name, gender, legs, hands, saying } = item) => {
+    print(`${species}; ${name}; ${gender}; ${legs}; ${hands}; ${saying}`);
+  }
+);
+
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
@@ -27,5 +79,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
